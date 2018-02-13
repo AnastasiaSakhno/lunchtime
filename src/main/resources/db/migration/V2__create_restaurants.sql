324f18658cd0b57c54d031354d2dc70962b61861
Anastasiia Sakhno <ask@anadeainc.com>
2018-02-13 20:00:44 +0200

    fix a typo

--- src/main/resources/db/migration/V2__create_restaurants.sql
+++ src/main/resources/db/migration/V2__create_restaurants.sql
@@ -2,7 +2,7 @@ CREATE TABLE restaurants (
   id BIGSERIAL PRIMARY KEY,
   name VARCHAR(50) UNIQUE,
   address VARCHAR(255),
-  archieve BOOL NOT NULL DEFAULT FALSE
+  archive BOOL NOT NULL DEFAULT FALSE
 );
 
 CREATE UNIQUE INDEX restaurants_email_uindex ON restaurants (name);
--- src/main/resources/db/migration/V3__create_menu.sql
+++ src/main/resources/db/migration/V3__create_menu.sql
@@ -2,7 +2,7 @@ CREATE TABLE menu (
   id BIGSERIAL PRIMARY KEY,
   name VARCHAR(50) UNIQUE,
   restaurant_id BIGINT NOT NULL,
-  archieve BOOL NOT NULL DEFAULT FALSE,
+  archive BOOL NOT NULL DEFAULT FALSE,
   CONSTRAINT fk_projects_menu_id_restaurant_id FOREIGN KEY (restaurant_id) REFERENCES restaurants (id)
 );
 
