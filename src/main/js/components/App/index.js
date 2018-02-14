1d18d3327a6193d7f0b27636d97c15b09d952845
Anastasiia Sakhno <ask@anahoret.com>
2018-02-14 16:54:38 +0200

    use footer in app

--- src/main/js/components/App/index.js
+++ src/main/js/components/App/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import routes from '../routes'
+import Footer from '../Footer'
 
 const App = () => {
   return (
-    <div className='container-fluid'>
-      <Switch>
-        {routes.map((route, i) => <Route key={i} {...route} />)}
-      </Switch>
+    <div className='app'>
+      <main role='main'>
+        <div className='container'>
+          <Switch>
+            {routes.map((route, i) => <Route key={i} {...route} />)}
+          </Switch>
+        </div>
+        <Footer/>
+      </main>
     </div>
   )
 }
