6c71d931396044a465453a5aebd7f33f9571cefe
Anastasiia Sakhno <ask@anahoret.com>
2018-02-14 16:51:39 +0200

    added footer sketch

---
+++ src/main/js/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="container-fluid">
+      <p className="float-right"><a href="#">Back to top</a></p>
+      <p>© Anadea, Inc.</p>
+    </footer>
+  )
+}
+
+export default Footer
