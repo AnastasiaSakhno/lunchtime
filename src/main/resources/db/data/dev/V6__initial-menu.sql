d2fe56c6e16b2bea95390cb85edd949645956ce7
Anastasiia Sakhno <ask@anahoret.com>
2018-02-17 13:24:44 +0200

    fix wed to thu

--- src/main/resources/db/data/dev/V6__initial-menu.sql
+++ src/main/resources/db/data/dev/V6__initial-menu.sql
@@ -2,7 +2,7 @@ INSERT INTO menu (name, week_day, archive, restaurant_id)
   VALUES
     ('Loft', null, false, 1),
     ('Loft диета', null, false, 1),
-    ('Loft рыба', 'WED', false, 1),
+    ('Loft рыба', 'THU', false, 1),
     ('Loft мясо', null, false, 1),
     ('Loft веган', null, false, 1),
     ('LeGrand', null, false, 2),
