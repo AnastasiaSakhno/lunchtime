6e2c60c9492b1337071449a26be026507e78962f
Anastasiia Sakhno <ask@anahoret.com>
2018-02-17 13:23:30 +0200

    add seeds for menu

--- src/main/resources/db/data/dev/V6__initial-menu.sql
+++ src/main/resources/db/data/dev/V6__initial-menu.sql
@@ -1,3 +1,9 @@
--- INSERT INTO menu (name, restaurant_id, archive)
---   VALUES
---     ();
+INSERT INTO menu (name, week_day, archive, restaurant_id)
+  VALUES
+    ('Loft', null, false, 1),
+    ('Loft диета', null, false, 1),
+    ('Loft рыба', 'WED', false, 1),
+    ('Loft мясо', null, false, 1),
+    ('Loft веган', null, false, 1),
+    ('LeGrand', null, false, 2),
+    ('Primus', null, false, 3);
