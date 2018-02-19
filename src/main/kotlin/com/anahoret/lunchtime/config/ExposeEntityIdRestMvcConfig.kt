225074f98d3480ab239943d94150ffc86fe22d02
Anastasiia Sakhno <ask@anahoret.com>
2018-02-19 14:50:57 +0200

    return id for menu data rest + renamed mvc config class

--- src/main/kotlin/com/anahoret/lunchtime/config/ExposeEntityIdRestMvcConfig.kt
+++ src/main/kotlin/com/anahoret/lunchtime/config/ExposeEntityIdRestMvcConfig.kt
@@ -1,5 +1,6 @@
 package com.anahoret.lunchtime.config
 
+import com.anahoret.lunchtime.domain.Menu
 import com.anahoret.lunchtime.domain.Restaurant
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter
@@ -7,9 +8,9 @@ import org.springframework.stereotype.Component
 
 
 @Component
-class ExposeEntityIdRestMvcConfiguration : RepositoryRestConfigurerAdapter() {
+class ExposeEntityIdRestMvcConfig : RepositoryRestConfigurerAdapter() {
 
     override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration?) {
-        config!!.exposeIdsFor(Restaurant::class.java)
+        config!!.exposeIdsFor(Restaurant::class.java, Menu::class.java)
     }
 }
