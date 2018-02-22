621d9a16077441d2580274d5c38a19fc7068a68a
Anastasiia Sakhno <ask@anahoret.com>
2018-02-22 18:39:06 +0200

    add menu documents controller

---
+++ src/main/kotlin/com/anahoret/lunchtime/web/controllers/MenuDocumentsController.kt
@@ -0,0 +1,39 @@
+package com.anahoret.lunchtime.web.controllers
+
+import com.anahoret.lunchtime.domain.MenuDocument
+import com.anahoret.lunchtime.domain.MenuDocumentMetadata
+import com.anahoret.lunchtime.services.MenuDocumentService
+import org.springframework.http.*
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+import java.util.*
+
+@RestController
+@RequestMapping("/api/menu_documents")
+class MenuDocumentsController(var menuDocumentService: MenuDocumentService) {
+
+    @GetMapping
+    fun index(): HttpEntity<List<MenuDocumentMetadata>> {
+        val httpHeaders = HttpHeaders()
+        return ResponseEntity<List<MenuDocumentMetadata>>(menuDocumentService.findAllDocuments(), httpHeaders, HttpStatus.OK)
+    }
+
+    @GetMapping("/{id}")
+    fun show(@PathVariable id: String): HttpEntity<ByteArray> {
+        val httpHeaders = HttpHeaders()
+        httpHeaders.contentType = MediaType.ALL
+        return ResponseEntity(menuDocumentService.getDocumentFile(id), httpHeaders, HttpStatus.OK)
+    }
+
+    @PostMapping
+    @ResponseBody
+    fun upload(
+            @RequestParam(value = "file", required = true) file: MultipartFile,
+            @RequestParam(value = "restaurant", required = true) restaurant: String,
+            @RequestParam(value = "user", required = true) user: String): MenuDocumentMetadata {
+
+        val document = MenuDocument(fileData = file.bytes, fileName = file.originalFilename, restaurantName = restaurant, uploadedAt = Date(), userName = user)
+        menuDocumentService.save(document)
+        return document.metadata
+    }
+}
