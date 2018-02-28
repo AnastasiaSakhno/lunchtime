866de2e09b2a466fc2d3f59cee7dfd8f2c39590a
Anastasiia Sakhno <ask@anahoret.com>
2018-02-28 10:23:58 +0200

    add css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,39 @@
-var path = require('path')
+let path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const extractSass = new ExtractTextPlugin({
+  filename: './src/main/resources/static/built/[name].css'
+})
 
 module.exports = {
-    entry: './src/main/js/main.js',
-    devtool: 'sourcemaps',
-    cache: true,
-    output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                loader: 'babel-loader'
-            }
-        ]
-    }
+  entry: './src/main/js/main.js',
+  devtool: 'sourcemaps',
+  cache: true,
+  output: {
+    path: __dirname,
+    filename: './src/main/resources/static/built/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: path.join(__dirname, '.'),
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: extractSass.extract({
+          use: [{
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader'
+          }],
+          // use style-loader in development
+          fallback: 'style-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    extractSass
+  ]
 }
