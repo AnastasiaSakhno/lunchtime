453487b185fa523363599f2253437db3ac97df13
Anastasiia Sakhno <ask@anahoret.com>
2018-02-28 18:26:04 +0200

    add seeds for user day menu

--- src/main/resources/db/data/dev/V8__initial-user_day_menu.sql
+++ src/main/resources/db/data/dev/V8__initial-user_day_menu.sql
@@ -1,3 +1,6 @@
--- INSERT INTO user_day_menu (date, out, user_id, menu_id)
---   VALUES
---     ();
+INSERT INTO user_day_menu (date, out, archive, user_id, menu_id)
+  VALUES
+    ('2018-02-26', false, false, 2, 1),
+    ('2018-02-26', true, false, 3, 3),
+    ('2018-02-27', false, false, 2, 2),
+    ('2018-02-28', true, false, 2, 1);
