b88fb6780ff6ae52c27f03314b7c88f4a4ea3a61
Anastasiia Sakhno <ask@anadeainc.com>
2018-03-01 21:00:24 +0200

    add entity projections

---
+++ src/main/kotlin/com/anahoret/lunchtime/domain/projections/ShortUserProjection.kt
@@ -0,0 +1,12 @@
+package com.anahoret.lunchtime.domain.projections
+
+import com.anahoret.lunchtime.domain.User
+import org.springframework.data.rest.core.config.Projection
+
+@Projection(name = "short", types = [User::class])
+interface ShortUserProjection {
+
+    val id: Long
+    val fullName: String
+    val email: String
+}
