23f5ef253948993a4795cd132440b75d3aebd5b9
Anastasiia Sakhno <ask@anahoret.com>
2018-03-05 13:48:01 +0200

    remove user day menu seeds

--- src/main/resources/db/data/dev/V8__initial-user_day_menu.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO user_day_menu (date, out, archive, user_id, menu_id)
-  VALUES
-    ('2018-02-26', false, false, 2, 1),
-    ('2018-02-26', true, false, 3, 3),
-    ('2018-02-27', false, false, 2, 2),
-    ('2018-02-28', true, false, 2, 1);
