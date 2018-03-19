3429979feef77d0374e285d236569a2ccdc024d7
Anastasiia Sakhno <ask@anadeainc.com>
2018-03-19 11:17:16 +0200

    fix readonly user day menu out component

--- src/main/js/components/Regular/UsersMenu/UserDayMenu/UserDayMenuOut/ReadonlyUserDayMenuOut.js
+++ src/main/js/components/Regular/UsersMenu/UserDayMenu/UserDayMenuOut/ReadonlyUserDayMenuOut.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ReadonlyUserDayMenuOut = (out) =>
+const ReadonlyUserDayMenuOut = ({out}) =>
   <div className='float-left'>{out ? '(out)' : ''}</div>
 
 ReadonlyUserDayMenuOut.propTypes = {
