8e263b6d0d906f1c5d7856cce4c26a356f206636
Anastasiia Sakhno <ask@anadeainc.com>
2018-03-21 16:25:59 +0200

    use cancan branch HOC for header link components

---
+++ src/main/js/components/Header/HeaderLink/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {string} from 'prop-types'
+import {Link} from 'react-router-dom'
+import cancanBranch from '../../../HOC/branch/cancanBranch'
+import {Menu, MenuDocument, Restaurant, User} from '../../abilities'
+
+const PureHeaderLink = ({url, name}) => (
+  <li className="nav-item">
+    <Link className='nav-link' to={url}>{name}</Link>
+  </li>
+)
+
+PureHeaderLink.propTypes = {
+  url: string.isRequired,
+  name: string.isRequired
+}
+
+export const headerLink = (VerifiableClass) => (
+  cancanBranch({
+    action: 'view',
+    VerifiableClass: VerifiableClass,
+    CanComponent: PureHeaderLink
+  })()
+)
+
+export const RestaurantsHeaderLink = headerLink(Restaurant)
+export const MenuHeaderLink = headerLink(Menu)
+export const UsersHeaderLink = headerLink(User)
+export const MenuDocumentsHeaderLink = headerLink(MenuDocument)
--- src/main/js/components/Header/index.js
+++ src/main/js/components/Header/index.js
@@ -1,78 +1,44 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
+import {bool} from 'prop-types'
 import {connect} from 'react-redux'
-import LogoutLink from '../LogoutLink'
 import {Link} from 'react-router-dom'
-import withCurrentUser from '../../HOC/withCurrentUser'
-import {cancanUser, can, Menu, Restaurant, MenuDocument, User} from '../abilities'
 
-@withCurrentUser
-class Header extends Component {
-  render() {
-    const user = cancanUser(this.props.currentUser)
+import LogoutLink from '../LogoutLink'
+import {UsersHeaderLink, MenuHeaderLink, MenuDocumentsHeaderLink, RestaurantsHeaderLink} from './HeaderLink'
 
-    return (
-      <header>
-        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-          <a className="navbar-brand" href="/">Lunch time</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"/>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarCollapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <Link className='nav-link' to='/'>Home</Link>
-              </li>
-              {
-                can(user, 'view', User)
-                  ? <li className="nav-item">
-                    <Link className='nav-link' to='/admin/users'>Users</Link>
-                  </li>
-                  : ''
-              }
-              {
-                can(user, 'view', Restaurant)
-                  ? <li className="nav-item">
-                    <Link className='nav-link' to='/admin/restaurants'>Restaurants</Link>
-                  </li>
-                  : ''
-              }
-              {
-                can(user, 'view', Menu)
-                  ? <li className="nav-item">
-                    <Link className='nav-link' to='/admin/menu'>Menu</Link>
-                  </li>
-                  : ''
-              }
-              {
-                can(user, 'view', MenuDocument)
-                  ? <li className="nav-item">
-                    <Link className='nav-link' to='/admin/menu_documents'>Menu documents</Link>
-                  </li>
-                  : ''
-              }
-              <li className="nav-item">
-                {(this.props.authenticated) ? <LogoutLink/> : ''}
-              </li>
-            </ul>
-            <form className="form-inline mt-2 mt-md-0">
-              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-          </div>
-        </nav>
-        <br/>
-        <br/>
-      </header>
-    )
-  }
-}
-
-const {object, bool} = PropTypes
+const Header = ({authenticated}) => (
+  <header>
+    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <Link className='navbar-brand' to='/'>Lunch time</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+              aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"/>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link className='nav-link' to='/'>Home</Link>
+          </li>
+          <UsersHeaderLink url='/admin/users' name='Users'/>
+          <RestaurantsHeaderLink url='/admin/restaurants' name='Restaurants'/>
+          <MenuHeaderLink url='/admin/menu' name='Menu'/>
+          <MenuDocumentsHeaderLink url='/admin/menu_documents' name='Menu documents'/>
+          <li className="nav-item">
+            {(authenticated) ? <LogoutLink/> : ''}
+          </li>
+        </ul>
+        <form className="form-inline mt-2 mt-md-0">
+          <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <br/>
+    <br/>
+  </header>
+)
 
 Header.propTypes = {
-  currentUser: object,
   authenticated: bool.isRequired
 }
 
