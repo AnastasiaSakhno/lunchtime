a95b62ea7d6846170a831e026d0b88c3f3f001c9
Anastasiia Sakhno <ask@anadeainc.com>
2018-03-21 13:50:50 +0200

    refactoring menu form component: use state instead of dom

--- src/main/js/components/Admin/Menu/MenuForm/index.js
+++ src/main/js/components/Admin/Menu/MenuForm/index.js
@@ -1,61 +1,75 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, {Component} from 'react'
+import {array, func} from 'prop-types'
 
 class MenuForm extends Component {
   static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-    restaurants: PropTypes.array.isRequired
+    onSubmit: func.isRequired,
+    restaurants: array.isRequired
+  }
+
+  state = {
+    restaurant: null,
+    name: null,
+    weekDays: null
+  }
+
+  handleChange = (e) => {
+    const target = e.target
+    this.setState({...this.state, [target.name]: target.value.trim()})
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
 
-    const name = this.nameInput.value.trim()
-    const restaurantLink = this.restaurantSelect.value
-    if(!name || !restaurantLink) {
+    if (!this.state.name || !this.state.restaurant) {
       return
     }
 
-    const weekDays = this.weekDaysInput.value.trim()
+    this.props.onSubmit({
+      name: this.state.name,
+      weekDays: this.state.weekDays,
+      restaurant: this.state.restaurant,
+      archive: false
+    })
 
-    this.props.onSubmit({ name: name, weekDays: weekDays, restaurant: restaurantLink, archive: false })
-
-    this.weekDaysInput.value = this.nameInput.value = ''
+    e.target.reset()
   }
 
   render() {
     return (
       <div>
-        <form className='menu-form' onSubmit={ this.handleSubmit }>
+        <form className='menu-form' onSubmit={this.handleSubmit}>
           <div className="form-row align-items-center">
             <div className="col-auto">
               <label className="sr-only" htmlFor="restaurant_input">Restaurant</label>
-              <select className="custom-select mr-sm-2"
-                id='restaurant_input'
-                ref={ el => { this.restaurantSelect = el } }>
+              <select name='restaurant' className="custom-select mr-sm-2"
+                      id='restaurant_input'
+                      onChange={this.handleChange}>
                 <option>Select a Restaurant</option>
-                { this.props.restaurants.map((restaurant) => (
+                {this.props.restaurants.map((restaurant) => (
                   <option
-                    value={ restaurant._links.self.href }
-                    key={ `restaurant-option_${restaurant.id}` }>{ restaurant.name }</option>
-                )) }
+                    value={restaurant._links.self.href}
+                    key={`restaurant-option_${restaurant.id}`}>{restaurant.name}</option>
+                ))}
               </select>
             </div>
             <div className="col-auto">
               <label className="sr-only" htmlFor="name_input">Name</label>
               <input type='text'
-                className="form-control"
-                id="name_input"
-                placeholder='Name'
-                ref={ el => { this.nameInput = el } }/>
+                     name='name'
+                     className="form-control"
+                     id="name_input"
+                     placeholder='Name'
+                     onChange={this.handleChange}/>
             </div>
             <div className="col-auto">
               <label className="sr-only" htmlFor="week_days_input">Name</label>
               <input type='text'
-                className="form-control"
-                id='week_days_input'
-                placeholder='Week days'
-                ref={ el => { this.weekDaysInput = el } }/>
+                     name='weekDays'
+                     className="form-control"
+                     id='week_days_input'
+                     placeholder='Week days'
+                     onChange={this.handleChange}/>
             </div>
             <div className="col-auto">
               <button type="submit" className="btn btn-primary mr-sm-2">Add menu</button>
