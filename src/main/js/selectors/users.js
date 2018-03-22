075047623b1c5fedca12d940ba9b7891afa8104c
Anastasiia Sakhno <ask@anahoret.com>
2018-03-22 14:41:32 +0200

    fix ordered users selector

--- src/main/js/selectors/users.js
+++ src/main/js/selectors/users.js
@@ -1,4 +1,4 @@
-import { createSelector } from 'reselect'
+import {createSelector} from 'reselect'
 import {compose, filter, prepend, prop, sortBy, toLower} from 'ramda'
 
 import {getCurrentUser} from './auth'
@@ -9,7 +9,10 @@ export const orderedUsers = createSelector(
   [getCurrentUser, getUsers],
   (currentUser, users) => {
     let result = sortBy(compose(toLower, prop('fullName')))(users)
-    result = filter(u => u.id !== currentUser.id, result)
-    return prepend(currentUser, result)
+    if (currentUser) {
+      result = filter(u => u.id !== currentUser.id, result)
+      result = prepend(currentUser, result)
+    }
+    return result
   }
 )
