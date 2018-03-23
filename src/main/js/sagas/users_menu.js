12cfd446d4b00b575a9513a08070f32bd747978f
Anastasiia Sakhno <ask@anahoret.com>
2018-03-23 17:17:35 +0200

    refactoring users menu saga

--- src/main/js/sagas/users_menu.js
+++ src/main/js/sagas/users_menu.js
@@ -1,7 +1,7 @@
-import { takeLatest, takeEvery, put, call } from 'redux-saga/effects'
-import { get, post, putUserDayMenu, put as putRest } from '../utils/rest'
-import { sessionService } from 'redux-react-session'
-import moment from 'moment'
+import {takeLatest, takeEvery, put, call} from 'redux-saga/effects'
+import {get, post, putUserDayMenu, put as putRest} from '../utils/rest'
+import {sessionService} from 'redux-react-session'
+import {weekDateFormattedFromObject} from '../utils/date'
 
 import {USERS_MENU_URI, USERS_MENU_SEARCH_URI, USERS_MENU_BY_ID_URI} from '../utils/api'
 import actions from '../actions'
@@ -9,65 +9,46 @@ import * as actionTypes from '../actions/types'
 
 const loadUser = sessionService.loadUser
 
-const userDayMenuWithLinks = (udm, nudm) => ({
-  ...nudm,
-  user: {
-    _links: {
-      self: {
-        href: udm.user
-      }
-    }
-  },
-  menu: {
-    _links: {
-      self: {
-        href: udm.menu
-      }
-    }
-  }
-})
-
 const weekRange = (startDate) => {
-  const weekStart = startDate.day(1).format('YYYY-MM-DD')
-  const weekEnd = startDate.day(5).format('YYYY-MM-DD')
-  return { from: weekStart, to: weekEnd }
+  const weekStart = weekDateFormattedFromObject(startDate, 1)
+  const weekEnd = weekDateFormattedFromObject(startDate, 5)
+  return {from: weekStart, to: weekEnd}
 }
 
-export function* loadUsersMenu({ startDate }) {
-  const range = weekRange(startDate)
+function* loadUserDayMenu(startDate, plusWeeks = 0) {
+  const range = weekRange(startDate.add(plusWeeks, 'weeks'))
   const url = USERS_MENU_SEARCH_URI(range)
   const usersMenu = yield call(get, url)
-  yield put(actions.usersMenu.loaded({ startDate: range.from, userDayMenus: usersMenu }))
+  yield put(actions.usersMenu.loaded({startDate: range.from, userDayMenus: usersMenu}))
 }
 
-export function* loadPrevWeek({ startDate }) {
-  const range = weekRange(startDate.add(-7, 'days'))
-  const url = USERS_MENU_SEARCH_URI(range)
-  const usersMenu = yield call(get, url)
-  yield put(actions.usersMenu.loaded({ startDate: range.from, userDayMenus: usersMenu }))
+export function* loadUsersMenu({startDate}) {
+  yield loadUserDayMenu(startDate)
 }
 
-export function* loadNextWeek({ startDate }) {
-  const range = weekRange(startDate.add(7, 'days'))
-  const url = USERS_MENU_SEARCH_URI(range)
-  const usersMenu = yield call(get, url)
-  yield put(actions.usersMenu.loaded({ startDate: range.from, userDayMenus: usersMenu }))
+export function* loadPrevWeek({startDate}) {
+  yield loadUserDayMenu(startDate, -1)
 }
 
-export function* addUserDayMenu({ userDayMenu: udm }) {
+export function* loadNextWeek({startDate}) {
+  yield loadUserDayMenu(startDate, 1)
+}
+
+export function* addUserDayMenu({userDayMenu: udm}) {
   const user = yield call(loadUser)
   const newUserDayMenu = yield call(post, USERS_MENU_URI, user.auth_token, udm)
 
-  if(newUserDayMenu.id) {
-    yield put(actions.usersMenu.addedSuccessfully(userDayMenuWithLinks(udm, newUserDayMenu)))
+  if (newUserDayMenu.id) {
+    const usersMenu = yield call(get, USERS_MENU_BY_ID_URI({id: newUserDayMenu.id}))
+    yield put(actions.usersMenu.addedSuccessfully(usersMenu))
   }
 }
 
-export function* updateUserDayMenu({ userDayMenu: udm }) {
+export function* updateUserDayMenu({userDayMenu: udm}) {
   const user = yield call(loadUser)
   const response = yield call(putUserDayMenu, user.auth_token, udm)
 
-  if(response.status === 204) {
+  if (response.status === 204) {
     // TODO think how to change it only for None menu
     yield put(actions.usersMenu.updateOut({id: udm.id, out: false, date: udm.date}))
     const usersMenu = yield call(get, USERS_MENU_BY_ID_URI({id: udm.id}))
@@ -75,11 +56,11 @@ export function* updateUserDayMenu({ userDayMenu: udm }) {
   }
 }
 
-export function* updateUserDayMenuOut({ userDayMenu: udm }) {
+export function* updateUserDayMenuOut({userDayMenu: udm}) {
   const user = yield call(loadUser)
   const newUserDayMenu = yield call(putRest, `${USERS_MENU_URI}/${udm.id}`, user.auth_token, udm)
 
-  if(newUserDayMenu.id) {
+  if (newUserDayMenu.id) {
     yield put(actions.usersMenu.outUpdatedSuccessfully(newUserDayMenu))
   }
 }
