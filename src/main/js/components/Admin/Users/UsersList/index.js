84d3f5991f00dd3d20c5cd58482c283b46b05bfe
Anastasiia Sakhno <ask@Anastasiias-MacBook-Pro.local>
2018-07-17 19:15:50 +0300

    fix user component columns

--- src/main/js/components/Admin/Users/UsersList/index.js
+++ src/main/js/components/Admin/Users/UsersList/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { string, number, arrayOf, shape } from 'prop-types'
+import { string, number, arrayOf, shape, array } from 'prop-types'
 
 import { User } from '../../Users'
 import withSpinner from '../../../../HOC/withSpinner'
@@ -33,8 +33,8 @@ UsersList.propTypes = {
     shape({
       id: number,
       fullName: string,
-      email: string,
-      role: string
+      username: string,
+      roles: array
     })
   ).isRequired
 }
