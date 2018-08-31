8d0b8072dde085a1c7b34e001e517d29f093945d
Anastasiia Sakhno <ask@anahoret.com>
2018-08-31 14:40:19 +0300

    only anadeainc domain is allowed (filter in connection sign up)

--- src/main/kotlin/com/anahoret/lunchtime/web/handlers/social/AutoSignUpHandler.kt
+++ src/main/kotlin/com/anahoret/lunchtime/web/handlers/social/AutoSignUpHandler.kt
@@ -18,15 +18,18 @@ class AutoSignUpHandler(
 
     @Transactional
     override fun execute(connection: Connection<*>) =
-        User().also {
-            val profile = connection.fetchUserProfile()
-            it.setUsername(profile.email)
-            it.fullName = profile.name
-            it.providerId = connection.key.providerId
-            it.providerUserId = connection.key.providerUserId
-            grantRoles(it)
-            userRepository.save(it)
-        }.userId
+        connection.fetchUserProfile()
+            .takeIf { it.email.matches(DOMAIN_REGEX) }
+            ?.let { profile ->
+                User().also {
+                    it.setUsername(profile.email)
+                    it.fullName = profile.name
+                    it.providerId = connection.key.providerId
+                    it.providerUserId = connection.key.providerUserId
+                    grantRoles(it)
+                    userRepository.save(it)
+                }.userId
+            } ?: throw IllegalStateException("Wrong domain. Only \"$DOMAIN_STRING\" is allowed")
 
     private fun grantRoles(user: User) {
         user.grantAuthority(authorityByName("ROLE_REGULAR"))
@@ -38,4 +41,10 @@ class AutoSignUpHandler(
     }
 
     private fun authorityByName(name: String) = authorityRepository.findByName(name)!!
+
+    companion object {
+        private const val DOMAIN_STRING = "anadeainc.com"
+        private const val DOMAIN_PATTERN = "^.*@$DOMAIN_STRING$"
+        private val DOMAIN_REGEX = Regex(DOMAIN_PATTERN)
+    }
 }
